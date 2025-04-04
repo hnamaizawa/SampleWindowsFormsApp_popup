906b4c33c9df0cf3ebc1585e846c5377f2222c8f
hnamaizawa <hnamaizawa@gmail.com>
2025-04-04 13:42:12 +0900

    プロジェクト ファイルを追加します。

---
+++ SampleWindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace SampleWindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dateTimePickerDeliveryDate = new System.Windows.Forms.DateTimePicker();
+            this.txtCustomer = new System.Windows.Forms.TextBox();
+            this.txtDeliveryAddress = new System.Windows.Forms.TextBox();
+            this.txtWarehouse = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(42, 588);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "実行";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(36, 35);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 33);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "納品書登録";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(42, 383);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(662, 178);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
+            // 
+            // dateTimePickerDeliveryDate
+            // 
+            this.dateTimePickerDeliveryDate.Location = new System.Drawing.Point(42, 115);
+            this.dateTimePickerDeliveryDate.Name = "dateTimePickerDeliveryDate";
+            this.dateTimePickerDeliveryDate.Size = new System.Drawing.Size(200, 30);
+            this.dateTimePickerDeliveryDate.TabIndex = 5;
+            // 
+            // txtCustomer
+            // 
+            this.txtCustomer.Location = new System.Drawing.Point(42, 189);
+            this.txtCustomer.Name = "txtCustomer";
+            this.txtCustomer.Size = new System.Drawing.Size(382, 30);
+            this.txtCustomer.TabIndex = 6;
+            // 
+            // txtDeliveryAddress
+            // 
+            this.txtDeliveryAddress.Location = new System.Drawing.Point(42, 266);
+            this.txtDeliveryAddress.Name = "txtDeliveryAddress";
+            this.txtDeliveryAddress.Size = new System.Drawing.Size(382, 30);
+            this.txtDeliveryAddress.TabIndex = 7;
+            this.txtDeliveryAddress.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtWarehouse
+            // 
+            this.txtWarehouse.Location = new System.Drawing.Point(42, 334);
+            this.txtWarehouse.Name = "txtWarehouse";
+            this.txtWarehouse.Size = new System.Drawing.Size(382, 30);
+            this.txtWarehouse.TabIndex = 8;
+            this.txtWarehouse.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 23);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "納品日\n";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 23);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "得意先\n";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 240);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 23);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "届け先\n";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(40, 308);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 23);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "倉庫";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1505, 874);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtWarehouse);
+            this.Controls.Add(this.txtDeliveryAddress);
+            this.Controls.Add(this.txtCustomer);
+            this.Controls.Add(this.dateTimePickerDeliveryDate);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("MS UI Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DateTimePicker dateTimePickerDeliveryDate;
+        private System.Windows.Forms.TextBox txtCustomer;
+        private System.Windows.Forms.TextBox txtDeliveryAddress;
+        private System.Windows.Forms.TextBox txtWarehouse;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ SampleWindowsFormsApp/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SampleWindowsFormsApp
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+
+            SetupDataGridView();
+
+            // 初期表示時に1行追加
+            dataGridView1.Rows.Add();
+
+            // DataGridViewにフォーカスを設定
+            dataGridView1.CurrentCell = dataGridView1.Rows[0].Cells["商品コード"];
+            dataGridView1.BeginEdit(true);
+
+            // フォームのサイズを自動調整
+            this.Load += (s, e) =>
+            {
+                int width = dataGridView1.PreferredSize.Width;
+                int height = dataGridView1.PreferredSize.Height;
+                this.ClientSize = new System.Drawing.Size(width, height);
+            };
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var savedLocation = Properties.Settings.Default.FormLocation;
+
+            if (savedLocation != Point.Empty && IsLocationVisible(savedLocation))
+            {
+                this.Location = savedLocation;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.FormLocation = this.Location;
+            Properties.Settings.Default.Save();
+        }
+
+        private bool IsLocationVisible(Point location)
+        {
+            foreach (var screen in Screen.AllScreens)
+            {
+                if (screen.WorkingArea.Contains(location))
+                    return true;
+            }
+            return false;
+        }
+
+        // DataGridView初期設定
+        private void SetupDataGridView()
+        {
+            dataGridView1.ColumnCount = 6;
+            dataGridView1.ColumnHeadersDefaultCellStyle.WrapMode = DataGridViewTriState.False;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+
+
+            dataGridView1.Columns[0].Name = "商品コード";
+            dataGridView1.Columns[1].Name = "入数";
+            dataGridView1.Columns[2].Name = "入数単位";
+            dataGridView1.Columns[3].Name = "売上数";
+            dataGridView1.Columns[4].Name = "単位";
+            dataGridView1.Columns[5].Name = "総数量";
+
+            // すべての列を編集可能に設定
+            foreach (DataGridViewColumn col in dataGridView1.Columns)
+            {
+                col.ReadOnly = false;
+            }
+
+            // 入数単位用のComboBox列を設定
+            var unitColumn = new DataGridViewComboBoxColumn();
+            unitColumn.Name = "入数単位";
+            unitColumn.Items.AddRange("個", "箱", "セット");
+            dataGridView1.Columns.RemoveAt(2);
+            dataGridView1.Columns.Insert(2, unitColumn);
+
+            // 削除ボタンを追加
+            //var deleteButton = new DataGridViewButtonColumn();
+            //deleteButton.Name = "削除";
+            //deleteButton.Text = "削除";
+            //deleteButton.UseColumnTextForButtonValue = true;
+            //dataGridView1.Columns.Add(deleteButton);
+
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.AllowUserToDeleteRows = false;
+        }
+
+        // 行追加ボタン
+        private void buttonAddRow_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Add();
+        }
+
+        // 送信ボタン
+        private void buttonSubmit_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("データが送信されました。", "送信完了");
+        }
+
+        // DataGridViewでボタンがクリックされた時のイベント（行の削除処理）
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.Columns[e.ColumnIndex].Name == "削除")
+            {
+                if (e.RowIndex >= 0)
+                {
+                    dataGridView1.Rows.RemoveAt(e.RowIndex);
+                }
+            }
+        }
+
+        // 総数量の自動計算
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 &&
+                (dataGridView1.Columns[e.ColumnIndex].Name == "入数" ||
+                 dataGridView1.Columns[e.ColumnIndex].Name == "売上数"))
+            {
+                var row = dataGridView1.Rows[e.RowIndex];
+
+                if (int.TryParse(Convert.ToString(row.Cells["入数"].Value), out int packSize) &&
+                    int.TryParse(Convert.ToString(row.Cells["売上数"].Value), out int saleQty))
+                {
+                    row.Cells["総数量"].Value = packSize * saleQty;
+                }
+                else
+                {
+                    row.Cells["総数量"].Value = null;
+                }
+            }
+        }
+
+        // DataGridViewのセル入力後に即時変更を反映させるための処理
+        private void dataGridView1_CurrentCellDirtyStateChanged(object sender, EventArgs e)
+        {
+            if (dataGridView1.IsCurrentCellDirty)
+                dataGridView1.CommitEdit(DataGridViewDataErrorContexts.Commit);
+        }
+
+
+        // フォームの表示時に実行されるイベントハンドラ
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            // フォームが完全表示されたタイミングでサイズ調整
+            this.Size = new System.Drawing.Size(750, 700);
+
+            // ポップアップ表示
+            MessageBox.Show(
+                "日頃よりご利用いただきありがとうございます。システムの更なる向上を目指し、 以下の日程でバージョンアップに伴うシステムメンテナンスを実施いたします。\r\n\r\n" +
+                "メンテナンス日時: 2025年6月1日（日） 午前0:00 〜 午前6:00\r\n\r\n" +
+                "この間、システムをご利用いただけませんのでご注意ください。お客様にはご迷惑をお掛けしますが、 ご理解とご協力を賜りますようお願い申し上げます。\r\n\r\n",
+                "バージョンアップのお知らせ",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information
+            );
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();  // アプリケーションを終了する
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
